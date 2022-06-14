a2a7fa73fb4969c897dee9438150a864f474b6fb
Nishad Islam <nishad.nik@gmail.com>
2022-06-14 11:45:36 +0600

    auth module and service initialized

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { UserModule } from './user/user.module';
       'mongodb+srv://nishad:Nishad90zx@cluster0.wtjke.mongodb.net/?retryWrites=true&w=majority',
     ),
     UserModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
