67efd50f299510c253c6fd3b78995a3819e39292
Nishad Islam <nishad.nik@gmail.com>
2022-06-14 11:50:14 +0600

    auth service updated

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.getUser(email);
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!user) {
+      throw new NotAcceptableException('could not find the user');
+    }
+
+    if (user && passwordValid) {
+      return {
+        userId: user._id,
+        email: user.email,
+      };
+    }
+    return null;
+  }
+}
